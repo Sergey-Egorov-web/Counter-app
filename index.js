57bfda40f110fa2228bb6fcad0dcca8e21a4edde
Sergey Egorov <ispanezzz.foto@gmail.com>
2023-05-02 05:39:30 +0300

    add active/disactive button reset counter

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const ACTIVE_COUNTER_CLASSNAME = "active";
+
 let button = document.querySelector(".counter-plus");
 let buttonReset = document.querySelector(".counter-reset");
 let counterText = document.querySelector(".counter");
@@ -7,9 +9,11 @@ let counter = 0;
 button.addEventListener("click", function () {
   counter = counter + 1;
   counterText.innerText = counter;
+  buttonReset.classList.add(ACTIVE_COUNTER_CLASSNAME);
 });
 
 buttonReset.addEventListener("click", function () {
   counter = 0;
   counterText.innerText = counter;
+  buttonReset.classList.remove(ACTIVE_COUNTER_CLASSNAME);
 });
